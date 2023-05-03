c5fc35f8ae6e2ea985da74db76d8c8a2f5f52a31
Dan Webb <dan.webb@damacus.io>
2023-05-03 09:41:56 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,18 @@ license           'Apache-2.0'
 description       'Installs/Configures cinc-omnibus'
 version           '1.0.3'
 chef_version      '>= 16.0'
-issues_url        'https://github.com/sous-chefs/cinc-omnibus'
-source_url        'https://github.com/sous-chefs/cinc-omnibus/issues'
+issues_url        'https://github.com/sous-chefs/cinc-omnibus/issues'
+source_url        'https://github.com/sous-chefs/cinc-omnibus'
 
-%w(redhat centos scientific oracle amazon fedora debian ubuntu suse opensuseleap).each do |os|
-  supports os
-end
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'oracle'
+supports 'amazon'
+supports 'fedora'
+supports 'debian'
+supports 'ubuntu'
+supports 'suse'
+supports 'opensuseleap'
 
 depends 'chef-ingredient'
