9cc6d730dc40a87fd08bc30036628692166536c2
Lance Albertson <lance@osuosl.org>
2023-09-21 12:51:37 -0700

    Use correct linking for install path
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -140,13 +140,13 @@ cookbook_file '/usr/local/share/ruby-docker-copy-patch.rb'
 
 # Fix problems only on Debian 11 and older on ARM which results in:
 # make: /usr/bin/mkdir: No such file or directory
-# make: /usr/bin/install: No such file or directory
+# make: /bin/install: No such file or directory
 if arm? && debian_platform? && node['platform_version'].to_i < 12
   link '/usr/bin/mkdir' do
     to '/bin/mkdir'
   end
 
-  link '/usr/bin/install' do
-    to '/bin/install'
+  link '/bin/install' do
+    to '/usr/bin/install'
   end
 end
